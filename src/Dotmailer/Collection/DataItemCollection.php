560ad82b351e4ee658b0b721003cf998fdfb136f
Lee Willis <leewillis77@gmail.com>
2013-12-19 22:24:59 +0000

    Add DataItemCollection

---
+++ src/Dotmailer/Collection/DataItemCollection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dotmailer\Collection;
+
+/**
+ * A collection of Dotmailer\DataItem objects.
+ *
+ * @extends Dotmailer\Collection
+ */
+class DataItemCollection extends Collection
+{
+    protected $entity_type = 'Dotmailer\Entity\DataItem';
+}
