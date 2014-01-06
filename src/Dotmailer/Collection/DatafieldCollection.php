f9fb12bd64bb45e85532b65ea5cddca45db9f8bd
Lee Willis <lee@hydrant.co.uk>
2014-01-06 14:46:14 +0000

    Add hasField() to DatafieldCollection for convenienve

--- src/Dotmailer/Collection/DatafieldCollection.php
+++ src/Dotmailer/Collection/DatafieldCollection.php
@@ -10,4 +10,13 @@ namespace Dotmailer\Collection;
 class DatafieldCollection extends Collection
 {
     protected $entity_type = 'Dotmailer\Entity\Datafield';
+
+    public function hasField($field_name)
+    {
+      foreach ($this->collection as $field) {
+        if ($field->getName() == $field_name)
+          return true;
+      }
+      return false;
+    }
 }
